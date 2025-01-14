e09ed3ae04231557c76d49c05507349da9eb4d1a
relicary <relicary@gmail.com>
2025-01-14 12:14:13 +0100

    Basic project

---
+++ src/main/java/com/relicary/reactive_mongo/ReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package com.relicary.reactive_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoApplication.class, args);
+	}
+
+}
