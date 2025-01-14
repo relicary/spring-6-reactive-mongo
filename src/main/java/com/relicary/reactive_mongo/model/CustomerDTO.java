50431fb4fd75ad2b3d03e7b83c7278d6e4eec2be
relicary <relicary@gmail.com>
2025-01-14 13:17:53 +0100

    Added DTO for Beer and Customer

---
+++ src/main/java/com/relicary/reactive_mongo/model/CustomerDTO.java
@@ -0,0 +1,26 @@
+package com.relicary.reactive_mongo.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDTO {
+
+    private Integer id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String customerName;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
