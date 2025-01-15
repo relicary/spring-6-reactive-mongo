6e0d05dc53052d3af273270a83d911d67a054586
relicary <relicary@gmail.com>
2025-01-15 18:05:44 +0100

    MongoDB: Added Find Beer By Name in Repository layer

--- src/main/java/com/relicary/reactive_mongo/repositories/BeerRepository.java
+++ src/main/java/com/relicary/reactive_mongo/repositories/BeerRepository.java
@@ -2,6 +2,10 @@ package com.relicary.reactive_mongo.repositories;
 
 import com.relicary.reactive_mongo.domain.Beer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface BeerRepository extends ReactiveMongoRepository<Beer, String> {
+
+    Mono<Beer> findFirstByBeerName(String beerName);
+
 }
