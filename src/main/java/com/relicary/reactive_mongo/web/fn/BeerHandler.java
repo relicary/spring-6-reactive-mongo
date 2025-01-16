413a17eddf2ff41520c7476de3cb18ba19b40a32
relicary <relicary@gmail.com>
2025-01-16 18:03:08 +0100

    WebFlux.fn: Added the Hadler for Beer

---
+++ src/main/java/com/relicary/reactive_mongo/web/fn/BeerHandler.java
@@ -0,0 +1,21 @@
+package com.relicary.reactive_mongo.web.fn;
+
+import com.relicary.reactive_mongo.model.BeerDTO;
+import com.relicary.reactive_mongo.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
