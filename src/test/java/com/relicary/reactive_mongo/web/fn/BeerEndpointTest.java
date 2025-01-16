cecd8327d19da6a4a6696e997d3ae14fcfbc3c1a
relicary <relicary@gmail.com>
2025-01-16 18:04:08 +0100

    WebFlux.fn: Added Test for List Beers

---
+++ src/test/java/com/relicary/reactive_mongo/web/fn/BeerEndpointTest.java
@@ -0,0 +1,33 @@
+package com.relicary.reactive_mongo.web.fn;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.hamcrest.Matchers.greaterThan;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class BeerEndpointTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    @Order(2)
+    void testListBeers() {
+        webTestClient.get().uri(BeerRouterConfig.BEER_PATH)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody().jsonPath("$.size()").value(greaterThan(1));
+    }
+
+}
